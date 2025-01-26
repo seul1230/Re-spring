8dfdea7588a3a3e98e66b046c522686dc118e50a
김민철 <alscjf0327@naver.com>
2025-01-26 17:47:04 +0900

    feat(yesterday) : 어제 메인 화면 구현 위한 더미 데이터 생성
    어제 메인 화면 구성을 위한 더미 데이터를 생성하였습니다.

--- frontend/src/app/yesterday/page.tsx
+++ frontend/src/app/yesterday/page.tsx
@@ -1,10 +1,29 @@
 // page.tsx
+
+"use client"
+
 import React from "react";
+
+import {SearchBar} from "@/components/custom/SearchBar"
+import {bookData, iBook} from "@/mocks/book/book-mockdata"
+import {SearchBookCard} from "@/app/yesterday/components/SearchBookCard"
 
 export default function HomePage() {
   return (
     <div>
-      <h2>어제 페이지</h2>
+      <SearchBar defaultValue={""} placeholder="봄날의 서, 작가 검색.."></SearchBar>
+      <div className="grid grid-cols-1 md:grid-cols-3 items-center gap-5">
+          {
+              // profileData 내의 모든 데이터를 SearchProfileCard 형식으로 출력.
+              bookData.map(({title, content, cover_img, tag, like, view, created_at, updated_at} : iBook) => {
+                  return (
+                      <div key={title}>
+                          <SearchBookCard title={title} content={content} cover_img = {cover_img} tag = {tag} like = {like} view = {view} created_at = {created_at} updated_at = {updated_at}/>
+                      </div>
+                  )
+              })
+          }
+      </div>
     </div>
   );
 }
