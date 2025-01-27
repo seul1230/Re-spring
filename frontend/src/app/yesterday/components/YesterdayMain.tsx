2aeeb999c7f16d235b14d89fdecbb2fdd1404177
김민철 <alscjf0327@naver.com>
2025-01-27 15:11:45 +0900

    feat(UI) : 탭 컴포넌트 커스텀 추가.
    우리 서비스의 테마에 맞는 탭 컴포넌트를 추가하였습니다.
    기본 동작은 shadcn의 tabs와 동일하게 설정할 수 있습니다.

--- frontend/src/app/yesterday/components/YesterdayMain.tsx
+++ frontend/src/app/yesterday/components/YesterdayMain.tsx
@@ -1,5 +1,47 @@
+import { Card, CardContent } from "@/components/ui/card"
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel"
+  import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/custom/TabGreen"
+
+  
+
 export const YesterdayMain = () => {
     return (
-        <h1>This is YesterdayMainPage</h1>
+        <div className="flex flex-col items-center justify-center">
+            <Tabs defaultValue="today">
+            <TabsList>
+                <TabsTrigger value="today">오늘의 서</TabsTrigger>
+                <TabsTrigger value="subscribe">구독</TabsTrigger>
+            </TabsList>
+            <TabsContent value="today">오늘의 서</TabsContent>
+            <TabsContent value="subscribe">구독</TabsContent>
+            </Tabs>
+            {/* <Carousel className="w-full max-w-xs"
+              opts={{
+                align: "start",
+                loop: true,
+              }}>
+                <CarouselContent>
+                    {Array.from({ length: 5 }).map((_, index) => (
+                    <CarouselItem key={index}>
+                        <div className="p-1">
+                        <Card className="border-brand">
+                            <CardContent className="flex aspect-square items-center justify-center p-6">
+                                <span className="text-4xl font-semibold">{index + 1}</span>
+                            </CardContent>
+                        </Card>
+                        </div>
+                    </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel> */}
+        </div>
     )
 }
