7ab19ee3556dd768e014d81de7e9cc1a886bd80c
김민철 <alscjf0327@naver.com>
2025-01-28 23:08:52 +0900

    feat(API) : 이벤트 목록 불러오기 API 구현 및 테스트 페이지 구현
    getAllEvents 함수와 테스트할 페이지를 구현하였습니다.

--- frontend/src/lib/api/event.ts
+++ frontend/src/lib/api/event.ts
@@ -1,6 +1,22 @@
+// 이벤트 관련 API를 호출하는 함수의 모음.
 import axiosAPI from "./axios";
 
-export const getAllEvents = async (userId : string) => {
-    const response = await axiosAPI.get(`/events?userId=${userId}`)
-    return response.data;
+export interface Event{
+    id: number,
+    eventName: string,
+    occurredAt: Date,
+    category: string,
+    display: boolean
+}
+
+// user ID를 기반으로 해당 유저의 모든 이벤트 목록을 반환한다.
+export const getAllEvents = async (userId : string): Promise<Event[]> => {
+    try{
+        const response = await axiosAPI.get(`/events?userId=${userId}`)
+        return response.data;
+    } catch(error){
+        console.error('에러 발생 : ', error);
+        throw new Error("getAllEvents의 에러 발생");
+    }
+
 }
