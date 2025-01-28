58aa95b44392357e2952ab8bfabba983ce9b366b
김민철 <alscjf0327@naver.com>
2025-01-28 14:26:08 +0900

    feat(yesterday-create): 봄날의 서 작성 페이지 추가

---
+++ frontend/src/app/yesterday/create/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import React from "react";
+
+export default function CreatePage() {
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+        <div className="flex">
+            <button>1</button>
+            <button>1</button>
+            <button>1</button>
+        </div>
+        <div>
+            <button>1</button>
+            <button>1</button>
+            <button>1</button>
+        </div>
+    </div>
+  );
+}
