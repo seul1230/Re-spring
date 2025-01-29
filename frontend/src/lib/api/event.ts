508384c78e1ec31bcf40b630999557db6779ea4c
김민철 <alscjf0327@naver.com>
2025-01-29 17:32:25 +0900

    feat(API) : 각 API 호출의 입출력 관련 주석 추가

--- frontend/src/lib/api/event.ts
+++ frontend/src/lib/api/event.ts
@@ -20,6 +20,8 @@ export interface EventPostDto{
 }
 
 // user ID를 기반으로 해당 유저의 모든 이벤트 목록을 반환한다.
+// 입력 : user ID (String)
+// 출력 : Event 배열 (Event[])
 export const getAllEvents = async (userId : string): Promise<Event[]> => {
     try{
         const response = await axiosAPI.get(`/events?userId=${userId}`)
@@ -31,10 +33,12 @@ export const getAllEvents = async (userId : string): Promise<Event[]> => {
 }
 
 // 새로운 이벤트를 만든다. 이때 반환되는 값은 그 이벤트의 ID 값이다.
+// 입력 : 이벤트 POST 관련 데이터 (EventPostDto)
+// 출력 : 새로 만들어진 이벤트의 ID (number)
 export const makeEvent = async (eventPostData : EventPostDto) => {
     try{
         const response = await axiosAPI.post(`/events`, eventPostData)
-        return response.data;
+        return response.data; // 새로 만들어진 이벤트의 ID가 반환된다.
     } catch(error){
         console.error('에러 발생 : ', error)
         throw new Error("makeEvent의 에러 발생");
@@ -42,6 +46,8 @@ export const makeEvent = async (eventPostData : EventPostDto) => {
 }
 
 // 이벤트를 삭제한다. 이 때 이벤트 id와 유저 id를 받아서 처리한다.
+// 입력 : 이벤트 ID(number), 유저 ID(string)
+// 출력 : 삭제가 제대로 되었는지 여부 boolean
 export const deleteEvent = async (eventId : number, userId : string) : Promise<boolean> => {
     try{
         const response = await axiosAPI.delete(`/events/${eventId}`,
@@ -66,6 +72,8 @@ export const deleteEvent = async (eventId : number, userId : string) : Promise<b
 }
 
 // 이벤트를 업데이트 하는 함수. 이벤트 ID와 이벤트 POST DTO를 모두 받는다.
+// 입력 : 이벤트 ID(number), 이벤트 POST 관련 데이터 (EventPostDto)
+// 출력 : 업데이트가 제대로 되었는지 여부 boolean
 export const updateEvent = async (eventId : number, eventPostData : EventPostDto) : Promise<boolean> => {
     try{
         const response = await axiosAPI.patch(`/events/${eventId}`, eventPostData);
@@ -83,6 +91,8 @@ export const updateEvent = async (eventId : number, eventPostData : EventPostDto
 }
 
 // 어떤 사용자의 타임라인 내 이벤트 목록을 불러옵니다.
+// 입력 : 유저 ID(string)
+// 출력 : 타임라인 내 이벤트 배열(Event[])
 export const getTimelineEvents = async (userId : string) : Promise<Event[]> => {
     try{
         const response = await axiosAPI.get(`/events/timeline/${userId}`);
