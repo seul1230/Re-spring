288eca2b6b53a1abc4d073c24d6ffb5098989a04
김민철 <alscjf0327@naver.com>
2025-02-02 22:57:33 +0900

    feat(Book) : 스타일 제거, api 연동 과정 거치기 (아직 미완성)

--- frontend/src/app/yesterday/create-book/page.tsx
+++ frontend/src/app/yesterday/create-book/page.tsx
@@ -1,141 +1,144 @@
 "use client";
 
-import { useState } from "react";
-
-import mockData from "@/mocks/story/story-mock"
+import { useState, useEffect } from "react";
+import { getAllStories, Story } from "@/lib/api";
+import { Tags } from "lucide-react";
 
 export default function CreateBook() {
+  const [userId, setUserId] = useState<string>("");
+  const [stories, setStories] = useState<Story[]>([]);
+  const [selectedStories, setSelectedStories] = useState<Story[]>([]);
   const [step, setStep] = useState(1);
-  const [formData, setFormData] = useState({
-    title: "",
-    tags: [],
-    content: "",
-    images: [],
-  });
 
-  // 상태 업데이트 함수
-  const updateFormData = (field: string, value: any) => {
-    setFormData((prev) => ({ ...prev, [field]: value }));
-  };
+  const [msg, setMsg] = useState<string>("...");
 
-  // 이미지 업로드 핸들러
-  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      const files = Array.from(event.target.files);
-      updateFormData("images", [...formData.images, ...files]);
+  /*
+  export interface BookPostDto{
+    userId : string,
+    title : string,
+    content : string,
+    tag : string[],
+    storyIds : number[],
+}
+  */
+  const [bookTitle, setBookTitle] = useState<string>("");
+  const [bookContent, setBookContent] = useState<string>("");
+  const [bookTags, setBookTags] = useState<string[]>([]);
+  const [bookStoryIds, setBookStoryIds] = useState<number[]>([]);
+  const [bookCoverImg, setBookCoverImg] = useState<File>();
+
+  const handleStoriesGet = async () => {
+    try{
+      const result = await getAllStories(userId);
+
+      setStories(result);
+    }catch(error){
+      setMsg('에러 발생' + error);
     }
-  };
+  }
+
+  const handleTags = (event : React.ChangeEvent<HTMLInputElement>) => {
+    const tagParsed = event.target.value.split(',').map((tag) => tag.trim());
+    
+    setBookTags(tagParsed);
+  }
+
+  const handleImageUpload = (event : React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+
+    if(files){
+      setBookCoverImg(files[0]);
+    }
+  }
 
   const handleSubmit = () => {
     alert("봄날의 서 제작 완료!");
   }
 
   return (
-    <div className="flex flex-col items-center max-w-lg mx-auto p-4">
-      <div className="basis-1/7">
-        <div className="mt-4 flex justify-between">
-          {step === 1 && 
-            <div>
-              글조각 선택하기
-              <button className="px-4 py-2 bg-blue-500 text-white rounded" onClick={() => setStep(step + 1)}>
-                다음
-              </button>
-            </div>
-          }
+    <div className="flex flex-col items-center">
+      <div>
+        {step === 1 && 
+          <div>
+            글조각 선택하기
+            <button className="bg-brand" onClick={() => setStep(step + 1)}>
+              다음
+            </button>
+          </div>
+        }
 
-          {step === 2 && 
-            <div>
-              <button className="px-4 py-2 bg-gray-300 rounded" onClick={() => setStep(step - 1)}>
-                이전
-              </button>
-              봄날의 서 쓰기
-              <button className="px-4 py-2 bg-blue-500 text-white rounded" onClick={() => setStep(step + 1)}>
-                다음
-              </button>
-            </div>
-          }
+        {step === 2 && 
+          <div>
+            <button className="bg-brand" onClick={() => setStep(step - 1)}>
+              이전
+            </button>
+            봄날의 서 쓰기
+            <button className="bg-brand" onClick={() => setStep(step + 1)}>
+              다음
+            </button>
+          </div>
+        }
 
-          {step === 3 && 
-            <div>
-              <button className="px-4 py-2 bg-gray-300 rounded" onClick={() => setStep(step - 1)}>
-                이전
-              </button>
-              표지 선택
-              <button className="px-4 py-2 bg-blue-500 text-white rounded" onClick={() => setStep(step + 1)}>
-                다음
-              </button>
-            </div>
-          }
+        {step === 3 && 
+          <div>
+            <button className="bg-brand" onClick={() => setStep(step - 1)}>
+              이전
+            </button>
+            표지 선택
+            <button className="bg-brand" onClick={() => setStep(step + 1)}>
+              다음
+            </button>
+          </div>
+        }
 
-          {step === 4 && 
-            <div>
-              <button className="px-4 py-2 bg-gray-300 rounded" onClick={() => setStep(step - 1)}>
-                이전
-              </button>
-              미리보기
-              <button className="px-4 py-2 bg-green-500 text-white rounded" onClick={handleSubmit}>
-                편찬
-              </button>
-            </div>
-          }
-        </div>
+        {step === 4 && 
+          <div>
+            <button className="bg-brand" onClick={() => setStep(step - 1)}>
+              이전
+            </button>
+            미리보기
+            <button className="bg-brand" onClick={handleSubmit}>
+              편찬
+            </button>
+          </div>
+        }
       </div>
-      <div className="basis-6/7">
+      <div>
         {step === 1 && (
           <div>
+            <label>사용자 ID 입력 : </label>
+            <input value = {userId} onChange={(e) => (setUserId(e.target.value))}></input>
+            <button className="bg-brand" onClick={handleStoriesGet}>글 조각 가져오기</button>
+            {stories && (<div>
+              {stories.map((story, idx) => (
+                <div key={idx}>
+                  <input type='checkbox' id={story.title}/>
+                  <div>{story.title}</div>
+                  <div>{story.content}</div>
+                </div>
+              ))}
+            </div>)}
 
           </div>
         )}
 
         {step === 2 && (
           <div>
-            <label className="block font-semibold">제목</label>
-            <input
-              type="text"
-              className="w-full border p-2 rounded mt-2"
-              placeholder="글 제목을 입력하세요"
-              value={formData.title}
-              onChange={(e) => updateFormData("title", e.target.value)}
-            />
-            <label className="block font-semibold">태그 입력</label>
-            <input
-              type="text"
-              className="w-full border p-2 rounded mt-2"
-              placeholder="예: 기술, 일상, 개발"
-              value={formData.tags.join(", ")}
-              onChange={(e) =>
-                updateFormData("tags", e.target.value.split(",").map((tag) => tag.trim()))
-              }
-            />
-            <label className="block font-semibold mt-4">내용</label>
-            <textarea
-              className="w-full border p-2 rounded mt-2"
-              rows={4}
-              placeholder="글 내용을 입력하세요"
-              value={formData.content}
-              onChange={(e) => updateFormData("content", e.target.value)}
-            />
+            <label>제목</label>
+            <input type="text" value={bookTitle} onChange={(e) => setBookTitle(e.target.value)}/>
+            <br/>
+            <label>태그 입력</label>
+            <input placeholder="예: 청춘, 마지막, 퇴직" value={bookTags} onChange={handleTags}/>
+            <br/>
+            <label>내용</label>
+            <textarea rows={4} value={bookContent} onChange={(e) => setBookContent(e.target.value)}/>
           </div>
         )}
 
         {step === 3 && (
           <div>
-            <label className="block font-semibold">표지 이미지 선택</label>
-            <input
-              type="file"
-              accept="image/*"
-              multiple
-              className="w-full border p-2 rounded mt-2"
-              onChange={handleImageUpload}
-            />
-
-            <div className="mt-2">
-              {formData.images.length > 0 && (
-                <p className="text-sm text-gray-500">
-                  {formData.images.length}개의 이미지가 선택됨
-                </p>
-              )}
-            </div>
+            <label>표지 이미지 선택</label>
+            <input type="file" accept="image/*"onChange={handleImageUpload}/>
           </div>
         )}
       </div>
