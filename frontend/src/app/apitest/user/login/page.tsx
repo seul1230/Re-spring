bb1a84d818f89ff1ab17efe953e368c877cad177
김민철 <alscjf0327@naver.com>
2025-02-03 21:05:39 +0900

    feat(API) : 로그인 후 세션 정보(userId, nickname) 받는 함수, 테스트페이지 구현현

--- frontend/src/app/apitest/user/login/page.tsx
+++ frontend/src/app/apitest/user/login/page.tsx
@@ -1,19 +1,27 @@
 "use client"
 
 import { useState } from "react"
-import { login } from "@/lib/api"
+import { login, getSessionInfo } from "@/lib/api"
 
 export default function LoginTestPage() {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [userId, setUserId] = useState<string>("");
+    const [userNickname, setUserNickname] = useState<string>("");
     const [msg, setMsg] = useState<string>("");
 
     const handlePost = async () => {
         try{
-            const result = await login(email, password);
+            const loginResult = await login(email, password);
 
-            if(result === true){
+            if(loginResult === true){
                 setMsg("로그인 완료!");
+                const sessionResult = await getSessionInfo();
+
+                console.log(sessionResult)
+
+                setUserId(sessionResult.userId);
+                setUserNickname(sessionResult.userNickname);
             }else{
                 setMsg("로그인 실패..");
             }
@@ -25,13 +33,15 @@ export default function LoginTestPage() {
 
     return (
         <div>
-            <h1>로그인 테스트 페이지</h1>
+            <h1>로그인 / 세션 정보 불러오기 테스트 페이지</h1>
             <label>이메일 : </label>
             <input value={email} onChange={(e) => (setEmail(e.target.value))}></input>
             <label>비밀번호 : </label>
             <input value={password} onChange={(e) => (setPassword(e.target.value))}></input>
             <button onClick={handlePost}>POST</button>
             <h3>{msg}</h3>
+            <h3>세션에 저장된 유저 ID : {userId} </h3>
+            <h3>세션에 저장된 유저 닉네임 : {userNickname}</h3>
         </div>
     )
 }
--- frontend/src/lib/api/user.ts
+++ frontend/src/lib/api/user.ts
@@ -1,6 +1,10 @@
 // 유저 관련 API를 호출하는 함수 모음
-import { Truculenta } from "next/font/google";
 import axiosAPI from "./axios";
+
+export interface SessionInfo{
+    userNickname: string,
+    userId: string
+}
 
 // 사용자 회원가입 함수
 // 입력 : 유저 닉네임(string), 이메일(string), 비밀번호(string)
@@ -37,7 +41,7 @@ export const login = async (email : string, password : string) : Promise<boolean
         formData.append('email', email);
         formData.append('password', password);
 
-        const response = await axiosAPI.post('/user/login', formData, {withCredentials : false});
+        const response = await axiosAPI.post('/user/login', formData);
 
         console.log(response);
 
@@ -48,5 +52,22 @@ export const login = async (email : string, password : string) : Promise<boolean
         }
     }catch(error : any){
         throw new Error(error.response?.data?.message || "login 함수 에러 발생");
+    }
+}
+
+// 사용자 ID와 닉네임이 저장된 세션 정보를 받아오는 함수.
+// 이 함수를 사용하기 전에 먼저 위 login() 함수에서 response 성공 코드를 받아야 합니다.
+// 이후 아래 함수를 사용해서 session 정보를 받을 수 있습니다.
+// 입력 : X
+// 출력 : SessionInfo 정보.
+export const getSessionInfo = async () : Promise<SessionInfo> => {
+    try{
+        const response = await axiosAPI.get('/user/session');
+
+        console.log(response)
+
+        return response.data as SessionInfo;
+    }catch(error : any){
+        throw new Error(error.response?.data?.message || "getSessionInfos() 함수 호출 에러 발생!");
     }
 }
