1ef4157d0a04f3ceb386047e69e083e0e68d8944
김민철 <alscjf0327@naver.com>
2025-02-07 17:28:04 +0900

    빌드 오류 수정정

--- frontend/src/app/yesterday/components/SearchBookCard.tsx
+++ frontend/src/app/yesterday/components/SearchBookCard.tsx
@@ -2,11 +2,11 @@ import Image from 'next/image'
 import {iBook} from '@/mocks/book/book-mockdata'
 
 export const SearchBookCard = (props : iBook) => {
-    const {title, content, cover_img, tag, like, view, created_at, updated_at} = props;
+    const {title, content, coverImg, tags, like, view, created_at, updated_at} = props;
 
     return(
         <div className="rounded-[15px] border border-solid flex flex-col items-center justify-center">
-            <Image src={cover_img} alt={`${title}_img`} className="h-[200px]" height={160} width={100} />
+            <Image src={coverImg} alt={`${title}_img`} className="h-[200px]" height={160} width={100} />
             <h5>{title}</h5>
         </div>
     )
