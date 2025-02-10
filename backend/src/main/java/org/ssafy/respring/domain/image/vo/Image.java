dcd18caceab4d53a510bfd4bc7c372def69f8984
sungwook <respectwo@naver.com>
2025-02-10 20:05:42 +0900

    feat: imagetype, entityid로 필드명 변경

--- backend/src/main/java/org/ssafy/respring/domain/image/vo/Image.java
+++ backend/src/main/java/org/ssafy/respring/domain/image/vo/Image.java
@@ -18,17 +18,12 @@ public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long imageId;
+
     private String s3Key;
 
-    @ManyToOne
-    @JoinColumn(name = "post_id")
-    @JsonIgnore // 순환 참조 방지
-    private Post post;
+    @Enumerated(EnumType.STRING)
+    private ImageType imageType;
 
-    @ManyToOne
-    @JoinColumn(name = "story_id")
-    @JsonIgnore
-    private Story story;
-
-
+    private Long entityId;
 }
+
