f048081ada6c329cf27015848d22d4c93caa4cc3
Yeseul <pink0569@naver.com>
2025-02-11 17:58:36 +0900

    refactor: AI request & controller

--- backend/src/main/java/org/ssafy/respring/domain/ai/dto/request/AICompileRequest.java
+++ backend/src/main/java/org/ssafy/respring/domain/ai/dto/request/AICompileRequest.java
@@ -2,6 +2,13 @@ package org.ssafy.respring.domain.ai.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record AICompileRequest (
-  @NotBlank String message){
-}
+import java.util.LinkedHashMap;
+
+//public record AICompileRequest (
+//  @NotBlank String message){
+//}
+
+public record AICompileRequest(
+        @NotBlank LinkedHashMap<String, String> content
+) { }
+
