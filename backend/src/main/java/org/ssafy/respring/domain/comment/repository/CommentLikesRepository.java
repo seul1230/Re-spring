be7d55eb1df58580df06f8ba4b9505b872c671e2
SeungYeop <syt05342@naver.com>
2025-02-13 15:04:22 +0900

    feat: 베스트 댓글 로직 추가(좋아요 및 토글 기능 추가)

---
+++ backend/src/main/java/org/ssafy/respring/domain/comment/repository/CommentLikesRepository.java
@@ -0,0 +1,15 @@
+package org.ssafy.respring.domain.comment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.ssafy.respring.domain.comment.vo.CommentLikes;
+import org.ssafy.respring.domain.comment.vo.Comment;
+import org.ssafy.respring.domain.user.vo.User;
+
+import java.util.Optional;
+import java.util.List;
+
+public interface CommentLikesRepository extends JpaRepository<CommentLikes, Long> {
+    Optional<CommentLikes> findByUserAndComment(User user, Comment comment);
+    int countByComment(Comment comment); // 해당 댓글의 좋아요 수 조회
+    List<CommentLikes> findByUser(User user); // 사용자가 좋아요 누른 댓글 리스트 조회
+}
