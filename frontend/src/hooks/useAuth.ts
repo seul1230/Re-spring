078a0063b0d9fc44252b9f7168e25d84314ab0ef
Taehan Yoon <taehan.d.yoon@gmail.com>
2025-02-17 11:29:12 +0900

    훅에서 id 반환하게 변경

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -6,6 +6,7 @@ import { getSessionInfo } from "@/lib/api";
 export function useAuth(blockUnauthenticated : boolean) {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
   const [userNickname, setUserNickname] = useState<string>("");
+  const [userId, setUserId] = useState<string>("");
 
   useEffect(() => {
     const checkAuth = async () => {
@@ -13,16 +14,19 @@ export function useAuth(blockUnauthenticated : boolean) {
             try {
                 console.log("useAuth에서 로그인 인증 보냄")
                 const session = await getSessionInfo();
-                if(session && session.userNickname){
+                if(session && session.userNickname && session.userId){
                     setIsAuthenticated(true);
                     setUserNickname(session.userNickname);        
+                    setUserId(session.userId);        
                 }else{
                     setIsAuthenticated(false);
                     setUserNickname("");  
+                    setUserId("");  
                 }
               } catch (error) {
                 setIsAuthenticated(false);
                 setUserNickname("");
+                setUserId("");
               }
         }else{  // 실제 로그인 인증 기능을 사용하지 않는 경우.
             setIsAuthenticated(true);
@@ -33,5 +37,5 @@ export function useAuth(blockUnauthenticated : boolean) {
     checkAuth();
   }, []);
 
-  return { isAuthenticated, setIsAuthenticated, userNickname };  // setIsAuthenticated를 반환하여 외부에서 상태를 변경할 수 있도록 함.
+  return { isAuthenticated, setIsAuthenticated, userNickname, userId };  // setIsAuthenticated를 반환하여 외부에서 상태를 변경할 수 있도록 함.
 }
